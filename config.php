31621cf83fbb2704c86fabfbfdb63e4d9c9b6998
sali-salam <salihsalam144@gmail.com>
2025-03-25 16:11:22 +0530

    config update

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost"; // Change to your remote MySQL host when deploying
-$username = "root"; // Change to your database username
-$password = ""; // Change to your database password
-$database = "favmovie"; // Change to your database name
+$host = "sql12.freesqldatabase.com"; // Change to your remote MySQL host when deploying
+$username = "sql12769474"; // Change to your database username
+$password = "2EX3WEL93F"; // Change to your database password
+$database = "sql12769474"; // Change to your database name
 
 $conn = new mysqli($host, $username, $password, $database);
 
